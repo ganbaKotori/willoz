7272e2ea71873aa03be17f64eb2d073350223e52
alex <alexanderjamesramirez@gmail.com>
2021-10-19 23:41:26 -0700

    update navbar

--- client/src/App.js
+++ client/src/App.js
@@ -32,9 +32,10 @@ class App extends Component {
     <Container>
     <Navbar.Brand href="#home">Wolliz</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#home">Buy</Nav.Link>
+      <Nav.Link href="#features">Rent</Nav.Link>
+      <Nav.Link href="#pricing">Sell</Nav.Link>
+      <Nav.Link href="#pricing">Tool</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
@@ -45,42 +46,7 @@ class App extends Component {
       </Col>
     </Row>
   </Container>
-      <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
-      </div>
+
       </>
     );
   }
